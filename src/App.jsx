507ed95da91b3f79c6ce2105d323737aae24e52d
scvzerng <842494771@qq.com>
2017-06-11 21:46:40 +0800

    contextmenu

--- src/App.jsx
+++ src/App.jsx
@@ -39,7 +39,6 @@ export default class App extends React.Component {
         return (
             <div>
 
-                <ContextMenuTrigger id="some_unique_identifier">
                     <Menu
                         onClick={this.handleClick}
                         style={{ width: 240 }}
@@ -48,14 +47,24 @@ export default class App extends React.Component {
                         mode="inline"
                     >
                         <SubMenu key="sub1" title={<span><Icon type="mail" /><span>Navigation One</span></span>}>
+
                             <MenuItemGroup key="g1" title="Item 1">
-                                <Menu.Item key="1">Option 1</Menu.Item>
-                                <Menu.Item key="2">Option 2</Menu.Item>
+                                <Menu.Item key="1">
+                                    <ContextMenuTrigger id="some_unique_identifier">
+                                    Option 1
+                                    </ContextMenuTrigger>
+                                </Menu.Item>
+                                <Menu.Item key="2">
+                                    <ContextMenuTrigger id="some_unique_identifier">
+                                    Option 2
+                                    </ContextMenuTrigger>
+                                </Menu.Item>
                             </MenuItemGroup>
                             <MenuItemGroup key="g2" title="Item 2">
                                 <Menu.Item key="3">Option 3</Menu.Item>
                                 <Menu.Item key="4">Option 4</Menu.Item>
                             </MenuItemGroup>
+
                         </SubMenu>
                         <SubMenu key="sub2" title={<span><Icon type="appstore" /><span>Navigation Two</span></span>}>
                             <Menu.Item key="5">Option 5</Menu.Item>
@@ -72,7 +81,6 @@ export default class App extends React.Component {
                             <Menu.Item key="12">Option 12</Menu.Item>
                         </SubMenu>
                     </Menu>
-                </ContextMenuTrigger>
                 <ContextMenu id="some_unique_identifier">
                     <div style={{borderStyle:"solid",zIndex:9999}} >
                     <MenuItem data={"some_data"} onClick={this.handleClick}>
